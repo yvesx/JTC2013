ba53ff6edc2e5f8fe8e8c0ffbf368814b5f93f32
Yves Xie <yxi389@gmail.com>
2013-10-21 13:37:15 -0500

    generate output

--- solve.py
+++ solve.py
@@ -1,5 +1,6 @@
 # python solve.py n10stg.csv N
 # read the strategy. and solve the optimality of the strategy instead of running simuations.
+from __future__ import print_function
 import sys
 import os
 import string
@@ -13,6 +14,7 @@ import sim
 import scipy.sparse
 import scipy
 import scipy.sparse.linalg
+
 P = 0.37
 
 def solveSparseStrategy(stg,N):
@@ -48,6 +50,12 @@ def solveStrategy(stg,N):
 	x = numpy.linalg.solve(eqns, b)
 	return x
 
+def output(stg,q0,N,csv_file):
+	with open(csv_file, 'w') as the_file:
+		for i in xrange(N-1): # i = 0,1,...,N-2, X = 1,2,...,N-1
+			X = i+1
+			#stg[X] = [stg[X],q0[i]]
+			print("%u,%u,%.9f"%(X,stg[X],q0[i]), file=the_file)
 
 
 
@@ -59,5 +67,9 @@ if __name__ == "__main__":
 		q_vect = solveSparseStrategy(stg,N)
 	else:
 		q_vect = solveStrategy(stg,N)
-	print q_vect
-	print "Qs: %.9f"%((sum(q_vect)-1.0)/(N-1))
+	q0 = list(q_vect)
+	q0 = q0[1:]
+	q0 = q0[:-1]
+	output(stg,q0,N,sys.argv[1])
+	#print q0
+	#print "Qs: %.9f"%((sum(q_vect)-1.0)/(N-1))
